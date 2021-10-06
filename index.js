c223b43a8ad07c087eac86a2aa810bf49f242ce8
Stanley <stanley.jeong@shopify.com>
2021-10-06 14:37:34 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+
+/* Animate On Scroll */
+AOS.init({
+  duration: 1000
+});
+
+
+const navLinks = document.querySelectorAll('.nav-bar > a');
+
+const selectNav = id => {
+  navLinks.forEach(link => {
+    if (link.getAttribute('href') === id) {
+      link.classList.add('selected');
+    } else link.classList.remove('selected');
+  })
+};
+
+/* Smooth Scroll */
+const body = document.querySelector('body');
+body.addEventListener('click', e => {
+  if (e.target.matches('.external')) return;
+
+  e.preventDefault();
+
+  const href = e.target.closest('a').href;
+  if (href) {
+    selectNav(href);
+    const id = href.split('#')[1];
+    document.getElementById(id).scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
+});
+
+
+/* Waypoints */
+['jumbo', 'about', ].forEach(id => {
+  console.log(id)
+  new Waypoint({
+    element: document.getElementById(id),
+    handler: () => {selectNav('#' + id)}
+  })
+})
